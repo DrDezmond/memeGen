3a7f85bcf17725b6d762d69e88f3a659a2e33ab0
Artem Yeldinov <artem.yeldinov@kingfisher.com>
2022-10-27 16:59:39 +0300

    port changes

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,13 +1,19 @@
 package apiserver
 
+import "os"
+
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level"`
 }
 
 func NewConfig() *Config {
+	port, exists := os.LookupEnv("$PORT")
+	if !exists {
+		port = "8080"
+	}
 	return &Config{
-		BindAddr: ":8080",
+		BindAddr: ":" + port,
 		LogLevel: "debug",
 	}
 }
